30fef9b8e8787db16ea5e87a7417e47e87ae267b
Emma Lawler <emily.lawler@colorado.edu>
2012-09-19 22:30:55 -0600

    equation
    for the amount of money in revenue kickstarter gains per day

--- kickstarter_revenue.rb
+++ kickstarter_revenue.rb
@@ -1,7 +1,27 @@
-today = Time.now
+p today = Time.now
 
-kickstarter_started = Time.utc(2009,"apr",28)
+p kickstarter_started = Time.utc(2009,"apr",28)
 
-revenue_per_day = 14505.5030168818
+p today - kickstarter_started = 107214482.544316
+
+p seconds_since_kickstarter_started = 107214482.544316
+
+p seconds_in_one_day = 86400
+
+p seconds_since_kickstarter_started / seconds_in_one_day = 1240.90836278144
+
+p days_since_kickstarter_started = 1240.90836278144
+
+p amount_of_money_kickstarter_has_raised = 360000000
+
+p amount_of_money_kickstarter_has_raised / days_since_kickstarter_started = 290110.060337635
+
+p revenue_per_day = 290110.060337635
+
+p percentage_of_revenue_that_kickstarter_keeps = 0.05
+
+p revenue_per_day * percentage_of_revenue_that_kickstarter_keeps = 
+
+p revenue_per_day_that_kickastarter_can_keep = 14505.5030168818
 
 
